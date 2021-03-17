db2995bc8f91832a2dfb1bb2925c4e6ab9455e6d
jalovisko <letovnn@gmail.com>
2021-03-16 21:36:34 -0400

    Added gyroid unit cell into a cusomizable function

--- lattice_scripts/gyroid.py
+++ lattice_scripts/gyroid.py
@@ -1,32 +1,61 @@
 # Python
 import cadquery as cq
 
-# Gyroïd, all edges are splines on different workplanes.
+# BEGIN USER INPUT
 
 thickness = 0.1
-edge_points = [
-    [[3.54, 3.54], [1.77, 0.0], [3.54, -3.54]],
-    [[-3.54, -3.54], [0.0, -1.77], [3.54, -3.54]],
-    [[-3.54, -3.54], [0.0, -1.77], [3.54, -3.54]],
-    [[-3.54, -3.54], [-1.77, 0.0], [-3.54, 3.54]],
-    [[3.54, 3.54], [0.0, 1.77], [-3.54, 3.54]],
-    [[3.54, 3.54], [0.0, 1.77], [-3.54, 3.54]],
-]
-plane_list = ["XZ", "XY", "YZ", "XZ", "YZ", "XY"]
-offset_list = [-3.54, 3.54, 3.54, 3.54, -3.54, -3.54]
-edge_wire = (
-    cq.Workplane(plane_list[0]).workplane(offset=-offset_list[0]).spline(edge_points[0])
-)
-for i in range(len(edge_points) - 1):
-    edge_wire = edge_wire.add(
-        cq.Workplane(plane_list[i + 1])
-        .workplane(offset=-offset_list[i + 1])
-        .spline(edge_points[i + 1])
-    )
-surface_points = [[0, 0, 0]]
-plate_4 = cq.Workplane("XY").interpPlate(edge_wire, surface_points, thickness)
-print("plate_4.val().Volume() = ", plate_4.val().Volume())
-#plate_4 = plate_4.translate((0, 5 * 12, 0))
+unit_cell_size = 7
 
-#show_object(fin)
-#fin.val().exportStep('Gyroid_Cell.stp')
+# END USER INPUT
+
+# Gyroïd, all edges are splines on different workplanes.
+def createGyroid(self, thickness, unit_cell_size):
+    half_unit_cell_size = unit_cell_size / 2.0
+    quarter_unit_cell_size = unit_cell_size / 4.0
+    edge_points = [
+        [[half_unit_cell_size, half_unit_cell_size],
+         [quarter_unit_cell_size, 0.0],
+         [half_unit_cell_size, - half_unit_cell_size]],
+        [[- half_unit_cell_size, - half_unit_cell_size],
+         [0.0, - quarter_unit_cell_size],
+         [half_unit_cell_size, - half_unit_cell_size]],
+        [[- half_unit_cell_size, - half_unit_cell_size],
+         [0.0, - quarter_unit_cell_size],
+         [half_unit_cell_size, - half_unit_cell_size]],
+        [[- half_unit_cell_size, - half_unit_cell_size],
+         [- quarter_unit_cell_size, 0.0],
+         [- half_unit_cell_size, half_unit_cell_size]],
+        [[half_unit_cell_size, half_unit_cell_size],
+         [0.0, quarter_unit_cell_size],
+         [- half_unit_cell_size, half_unit_cell_size]],
+        [[half_unit_cell_size, half_unit_cell_size],
+         [0.0, quarter_unit_cell_size],
+         [- half_unit_cell_size, half_unit_cell_size]],
+    ]
+    plane_list = ["XZ", "XY", "YZ", "XZ", "YZ", "XY"]
+    offset_list = [- half_unit_cell_size,
+                   half_unit_cell_size,
+                   half_unit_cell_size,
+                   half_unit_cell_size,
+                   - half_unit_cell_size,
+                   - half_unit_cell_size]
+    edge_wire = (
+        cq.Workplane(plane_list[0])
+        .workplane(offset = - offset_list[0])
+        .spline(edge_points[0])
+    )
+    for i in range(len(edge_points) - 1):
+        edge_wire = edge_wire.add(
+            cq.Workplane(plane_list[i + 1])
+            .workplane(offset = - offset_list[i + 1])
+            .spline(edge_points[i + 1])
+        )
+    surface_points = [[0, 0, 0]]
+    plate_4 = (cq.Workplane("XY").interpPlate(edge_wire, surface_points, thickness))
+    return self.union(self.eachpoint(lambda loc: plate_4.val().located(loc), True))
+cq.Workplane.createGyroid = createGyroid
+
+pnts = [(0, 0)]
+gyroid = (cq.Workplane("XY")
+          .pushPoints(pnts)
+          .createGyroid(thickness, unit_cell_size))
