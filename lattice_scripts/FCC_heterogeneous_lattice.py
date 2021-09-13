48d132b78f125d50f132b045cc664b65c586910e
jalovisko <letovnn@gmail.com>
2021-09-13 11:53:16 -0400

    copypaste of bcc, removes diagonals

---
+++ lattice_scripts/FCC_heterogeneous_lattice.py
@@ -0,0 +1,26 @@
+from parfunlib.topologies.fcc import fcc_heterogeneous_lattice
+
+# USER INPUT
+
+unit_cell_size = 10
+min_strut_diameter = 1
+max_strut_diameter = 3
+min_node_diameter = 1.1
+max_node_diameter = 3.3
+Nx = 2
+Ny = 2
+Nz = 4
+
+# END USER INPUT
+
+# Register our custom plugin before use.
+cq.Workplane.fcc_heterogeneous_lattice = fcc_heterogeneous_lattice
+
+#result = unit_cell(unit_cell_size, strut_radius)
+result = fcc_heterogeneous_lattice(unit_cell_size,
+                                    min_strut_diameter, 
+                                    max_strut_diameter,
+                                    min_node_diameter,
+                                    max_node_diameter,
+                                    Nx, Ny, Nz,
+                                    rule = 'sin')
