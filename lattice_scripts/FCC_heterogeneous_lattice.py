17f7b1d6b5752f74964a90d5fbf78f66990e3b1a
jalovisko <letovnn@gmail.com>
2021-09-14 15:17:14 -0400

    linear testing

--- lattice_scripts/FCC_heterogeneous_lattice.py
+++ lattice_scripts/FCC_heterogeneous_lattice.py
@@ -3,13 +3,13 @@ from parfunlib.topologies.fcc import fcc_heterogeneous_lattice
 # USER INPUT
 
 unit_cell_size = 10
-min_strut_diameter = 0.6
-max_strut_diameter = 2.6
-min_node_diameter = 0.6
-max_node_diameter = 2.5
+min_strut_diameter = 0.5
+max_strut_diameter = 2.5
+min_node_diameter = 0.7
+max_node_diameter = 3.5
 Nx = 1
 Ny = 1
-Nz = 1
+Nz = 10
 
 # END USER INPUT
 
@@ -22,5 +22,6 @@ result = fcc_heterogeneous_lattice(unit_cell_size,
                                     max_strut_diameter,
                                     min_node_diameter,
                                     max_node_diameter,
-                                    Nx, Ny, Nz,
-                                    rule = 'sin')
+                                    Nx, Ny, Nz
+                                    #rule = 'sin'
+                                    )
