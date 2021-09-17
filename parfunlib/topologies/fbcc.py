2311f983884e226e9e2655f82b446e2f2d6130fc
jalovisko <letovnn@gmail.com>
2021-09-17 13:08:50 -0400

    fbcc made

--- parfunlib/topologies/fbcc.py
+++ parfunlib/topologies/fbcc.py
@@ -13,14 +13,18 @@
 ##############################################################################
 
 from ..commons import eachpointAdaptive
+from .bcc import bcc_diagonals
+from .bcc import create_nodes as create_bcc_nodes
 
-from math import hypot, acos, degrees
+from math import hypot, acos, degrees, hypot
 import numpy as np
 
 import cadquery as cq
 
 # Register our custom plugins before use.
 cq.Workplane.eachpointAdaptive = eachpointAdaptive
+cq.Workplane.bcc_diagonals = bcc_diagonals
+cq.Workplane.create_bcc_nodes = create_bcc_nodes
 
 def create_diagonal_strut(
 		location: cq.occ_impl.geom.Location,
@@ -48,23 +52,20 @@ def create_diagonal_strut(
 	-------
 		cq.occ_impl.shapes.Compound
 			a solid model of the strut
-		
 	"""
 	hypot2D = hypot(unit_cell_size, unit_cell_size)
-	hypot3D = hypot(hypot2D, unit_cell_size)
 	result = (
 		cq.Workplane()
 		.transformed(rotate = cq.Vector(angle_x, angle_y, 0))
 		.circle(radius)
-		.extrude(hypot3D)
+		.extrude(hypot2D)
 	)
 	return result.val().located(location)
 
-def bcc_diagonals(
-		unit_cell_size: np.float64,
-		strut_radius: np.float64) -> cq.cq.Workplane:
+def fbcc_diagonals(unit_cell_size: np.float64,
+					strut_radius: np.float64) -> cq.cq.Workplane:
 	"""
-	Creates a solid model of the diagonals in a BCC unit cell.
+	Creates a solid model of the diagonals in a FBCC unit cell.
 
 	Parameters
     ----------
@@ -72,20 +73,22 @@ def bcc_diagonals(
             point location of the strut
         unit_cell_size : np.float64
             unit cell size (in mm)
-		strut_radius: np.float64
+		strut_radius : np.float64
 			strut radius (in mm)
 	Returns
 	-------
-		cq.occ_impl.shapes.Compound
+		result : cq.occ_impl.shapes.Compound
 			a solid model of the strut
-		
 	"""
-	# In a cube ABCDA1B1C1D1 this is the angle C1AD
-	angle_C1AD = 90 - degrees(acos(3**-.5))
+	# In a cube ABCDA1B1C1D1 this is the angle ABA1
 	corner_points = unit_cell_size * np.array(
 		[(0, 0),
+		(0, 0),
+		(1, 0),
 		(1, 0),
 		(1, 1),
+		(1, 1),
+		(0, 1),
 		(0, 1)]
 	)
 	result = (
@@ -95,32 +98,47 @@ def bcc_diagonals(
 			create_diagonal_strut,
 			callback_extra_args = [
 				{"unit_cell_size": unit_cell_size,
-				"radius": strut_radius,
+				 "radius": strut_radius,
 				 "angle_x": - 45,
-				 "angle_y": angle_C1AD},
+				 "angle_y": 0},
 				{"unit_cell_size": unit_cell_size,
 				"radius": strut_radius,
+				 "angle_x": 0,
+				 "angle_y": 45},
+				{"unit_cell_size": unit_cell_size,
+				"radius": strut_radius,
+				 "angle_x": 0,
+				 "angle_y": - 45},
+				{"unit_cell_size": unit_cell_size,
+				 "radius": strut_radius,
 				 "angle_x": - 45,
-				 "angle_y": - angle_C1AD},
+				 "angle_y": 0},
+				{"unit_cell_size": unit_cell_size,
+				 "radius": strut_radius,
+				 "angle_x": 45,
+				 "angle_y": 0},
+				{"unit_cell_size": unit_cell_size,
+				 "radius": strut_radius,
+				 "angle_x": 0,
+				 "angle_y": - 45},
+				{"unit_cell_size": unit_cell_size,
+				 "radius": strut_radius,
+				 "angle_x": 45,
+				 "angle_y": 0},
 				{"unit_cell_size": unit_cell_size,
 				"radius": strut_radius,
-				 "angle_x": 45,
-				 "angle_y": - angle_C1AD},
-				{"unit_cell_size": unit_cell_size,
-				"radius": strut_radius,
-				 "angle_x": 45,
-				 "angle_y": angle_C1AD}
+				 "angle_x": 0,
+				 "angle_y": 45}
 				],
 			useLocalCoords = True
 		)
 	)
 	return result
 # Register our custom plugin before use.
-cq.Workplane.bcc_diagonals = bcc_diagonals
+cq.Workplane.fbcc_diagonals = fbcc_diagonals
 
-def bcc_vertical_struts(
-		unit_cell_size: np.float64,
-		strut_radius: np.float64) -> cq.cq.Workplane:
+def fbcc_vertical_struts(unit_cell_size: np.float64,
+						strut_radius: np.float64) -> cq.cq.Workplane:
 	"""
 	Creates vertical struts of a unit cell.
 
@@ -132,7 +150,7 @@ def bcc_vertical_struts(
 			strut radius (in mm)
 	Returns
 	-------
-		result: cq.cq.Workplane
+		result : cq.cq.Workplane
 			a solid model of the union of all vertical struts
 	"""
 	result = cq.Workplane("XY")
@@ -153,9 +171,9 @@ def bcc_vertical_struts(
 				  )
 	return result
 # Register our custom plugin before use.
-cq.Workplane.bcc_vertical_struts = bcc_vertical_struts
+cq.Workplane.fbcc_vertical_struts = fbcc_vertical_struts
 
-def bcc_bottom_horizontal_struts(unit_cell_size, strut_radius):
+def fbcc_bottom_horizontal_struts(unit_cell_size, strut_radius):
 	result = cq.Workplane("XY")
 	angle = 90
 	corner_points = unit_cell_size * np.array(
@@ -177,9 +195,34 @@ def bcc_bottom_horizontal_struts(unit_cell_size, strut_radius):
 		angle += 90
 	return result
 # Register our custom plugin before use.
-cq.Workplane.bcc_bottom_horizontal_struts = bcc_bottom_horizontal_struts
+cq.Workplane.fbcc_bottom_horizontal_struts = fbcc_bottom_horizontal_struts
 
-def bcc_top_horizontal_struts(unit_cell_size, strut_radius):
+def fbcc_horizontal_diagonal_struts(unit_cell_size, strut_radius):
+	result = cq.Workplane("XY")
+	corner_points = unit_cell_size * np.array(
+		[(0, 0, 0),
+		(1, 0, 0),
+		(1, 1, 1),
+		(0, 1, 1)]
+	)
+	angle = 135.0
+	hypot2D = hypot(unit_cell_size, unit_cell_size)
+	for point in corner_points:
+		result = (result
+				  .union(
+					  cq.Workplane()
+					  .transformed(offset = cq.Vector(point[0], point[1], point[2]),
+								   rotate = cq.Vector(90, angle, 0))
+					  .circle(strut_radius)
+					  .extrude(hypot2D)
+					  )
+				  )
+		angle += 90
+	return result
+# Register our custom plugin before use.
+cq.Workplane.fbcc_horizontal_diagonal_struts = fbcc_horizontal_diagonal_struts
+
+def fbcc_top_horizontal_struts(unit_cell_size, strut_radius):
 	result = cq.Workplane("XY")
 	angle = 90
 	corner_points = unit_cell_size * np.array(
@@ -201,7 +244,7 @@ def bcc_top_horizontal_struts(unit_cell_size, strut_radius):
 		angle += 90
 	return result
 # Register our custom plugin before use.
-cq.Workplane.bcc_top_horizontal_struts = bcc_top_horizontal_struts
+cq.Workplane.fbcc_top_horizontal_struts = fbcc_top_horizontal_struts
 
 # Creates 4 nodes at the XY plane of each unit cell
 def create_nodes(node_diameter,
@@ -215,7 +258,8 @@ def create_nodes(node_diameter,
 		[(0, 0),
 		(1, 0),
 		(1, 1),
-		(0, 1)]
+		(0, 1),
+		(0.5, 0.5)]
 	)
 	for point in corner_points:
 		result= (result
@@ -241,41 +285,48 @@ def create_nodes(node_diameter,
 					  )
 				  )
 	half_unit_cell_size = unit_cell_size / 2
-	result= (result
-			 .union(
-				 cq.Workplane()
-				 .transformed(offset = cq.Vector(half_unit_cell_size,
-												 half_unit_cell_size,
-												 half_unit_cell_size))
-				 .box(added_node_diameter, added_node_diameter, added_node_diameter)
-				 .edges("|Z")
-				 .fillet(node_radius)
-				 .edges("|X")
-				 .fillet(node_radius)
-				 )
-			 )
+	middle_points = unit_cell_size * np.array(
+		[(0.5, 0),
+		(1, 0.5),
+		(0.5, 1),
+		(0, 0.5)]
+	)
+	for point in middle_points:
+		result= (result
+				  .union(
+					  cq.Workplane()
+					  .transformed(offset = cq.Vector(point[0], point[1], half_unit_cell_size))
+					  .box(added_node_diameter, added_node_diameter, added_node_diameter)
+					  .edges("|Z")
+					  .fillet(node_radius)
+					  .edges("|X")
+					  .fillet(node_radius)
+					  )
+				  )
+
 	return result
 cq.Workplane.create_nodes = create_nodes
 
-def unit_cell(location, unit_cell_size, strut_radius, node_diameter, type):
+def unit_cell(location, unit_cell_size, strut_radius, node_diameter):
 	result = cq.Workplane("XY")
-	result = result.union(bcc_diagonals(unit_cell_size, strut_radius))
-	#result = result.union(bcc_vertical_struts(unit_cell_size, strut_radius))
-	#result = result.union(bcc_bottom_horizontal_struts(unit_cell_size, strut_radius))
-	#result = result.union(bcc_top_horizontal_struts(unit_cell_size, strut_radius))
-	if type == 'bccz':
-		result = result.union(bcc_vertical_struts(unit_cell_size, strut_radius))
-	result = result.union(create_nodes(node_diameter, unit_cell_size))
+	result = (result
+			  .union(fbcc_diagonals(unit_cell_size, strut_radius))
+			  .union(fbcc_vertical_struts(unit_cell_size, strut_radius))
+			  .union(fbcc_horizontal_diagonal_struts(unit_cell_size, strut_radius))
+			  .union(bcc_diagonals(unit_cell_size, strut_radius))
+			  .union(create_nodes(node_diameter, unit_cell_size))
+			  .union(create_bcc_nodes(node_diameter, unit_cell_size))
+			  )
 	return result.val().located(location)
 cq.Workplane.unit_cell = unit_cell
 
-def bcc_heterogeneous_lattice(unit_cell_size,
+def fbcc_heterogeneous_lattice(unit_cell_size,
 							  min_strut_diameter,
 							  max_strut_diameter,
 							  min_node_diameter,
 							  max_node_diameter,
 							  Nx, Ny, Nz,
-							  type = 'bcc',
+							  type = 'fbcc',
 							  rule = 'linear'):
 	min_strut_radius = min_strut_diameter / 2.0
 	max_strut_radius = max_strut_diameter / 2.0
@@ -305,8 +356,7 @@ def bcc_heterogeneous_lattice(unit_cell_size,
 		for j in range(Nz):
 			unit_cell_params.append({"unit_cell_size": unit_cell_size,
 				"strut_radius": strut_radii[j],
-				"node_diameter": node_diameters[j],
-				"type": type})
+				"node_diameter": node_diameters[j]})
 	result = result.eachpointAdaptive(unit_cell,
 									  callback_extra_args = unit_cell_params,
 									  useLocalCoords = True)
