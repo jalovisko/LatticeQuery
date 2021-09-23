05638c24fec8f80d47cba74acf86030dbd0fe653
jalovisko <letovnn@gmail.com>
2021-09-23 16:11:13 -0400

    full schwartz unit cell

--- parfunlib/topologies/schwartz.py
+++ parfunlib/topologies/schwartz.py
@@ -8,8 +8,8 @@ import cadquery as cq
 # Register our custom plugins before use.
 cq.Workplane.eachpointAdaptive = eachpointAdaptive
 
-# Schwartz P surface, all edges are splines on different workplanes.
-def gyroid_000(self, thickness, unit_cell_size):
+# Schwartz P surface, all edges are arcs on different workplanes.
+def schwartz_000(self, thickness, unit_cell_size):
     delta_radius = 0.5 - 0.5/sqrt(2)
     convex_pnts = [[0.0, 0.5],
                 [delta_radius, delta_radius],
@@ -53,7 +53,7 @@ def gyroid_000(self, thickness, unit_cell_size):
     plate_4 = plate_4.interpPlate(edge_wire, surface_points, thickness)
     return self.union(self.eachpoint(lambda loc: plate_4.val().located(loc), True))
 
-cq.Workplane.gyroid_000 = gyroid_000
+cq.Workplane.schwartz_000 = schwartz_000
 
 def unit_cell(location, thickness, unit_cell_size,
 				delta = 1e-8 # a small tolerance (1e-10 is too small)
@@ -61,49 +61,29 @@ def unit_cell(location, thickness, unit_cell_size,
     half_unit_cell_size = unit_cell_size / 2.0
     # Octante 000
     pnts = [tuple(unit_cell_size / 2 for i in range(3))]
-    cq.Workplane.gyroid_000 = gyroid_000
-    g_000 = (cq.Workplane("XY").pushPoints(pnts)
-             .gyroid_000(thickness, unit_cell_size))
-    result = g_000
+    cq.Workplane.schwartz_000 = schwartz_000
+    s_000 = (cq.Workplane("XY").pushPoints(pnts)
+             .schwartz_000(thickness, unit_cell_size))
+    result = s_000
     # Octante 100
-    mirZY_pos = g_000.mirror(mirrorPlane = "ZY",
+    s_100 = s_000.mirror(mirrorPlane = "ZY",
                               basePointVector = (unit_cell_size, 0, 0))
-    g_100 = mirZY_pos.mirror(mirrorPlane = "XZ",
-                             basePointVector = (0, half_unit_cell_size, 0))
-    result = result.union(g_100)
+    result = result.union(s_100)
     # Octante 110
-    g_000_inverse = (cq.Workplane("XY").pushPoints(pnts)
-                     .gyroid_000(- thickness, unit_cell_size))
-    mirXZ_pos = g_000_inverse.mirror(mirrorPlane = "XZ",
-                                     basePointVector = (0, unit_cell_size, 0))
-    g_110 = mirXZ_pos.translate((unit_cell_size, 0, 0))
-    result = result.union(g_110)
+    #s_000_inverse = (cq.Workplane("XY").pushPoints(pnts)
+    #                 .schwartz_000(- thickness, unit_cell_size))
+    s_110 = s_100.mirror(mirrorPlane = "XZ",
+                            basePointVector = (0, unit_cell_size, 0))
+    #s_110 = mirXZ_pos.translate((unit_cell_size, 0, 0))
+    result = result.union(s_110)
     # Octante 010
-    mirYZ_neg = g_110.mirror(mirrorPlane = "YZ",
-                             basePointVector = (unit_cell_size, 0, 0))
-    g_010 = mirYZ_neg.mirror(mirrorPlane = "XZ",
-                             basePointVector = (0, 1.5 * unit_cell_size, 0))
-    result = result.union(g_010)
-    # Octante 001
-    g_001 = g_110.translate((-unit_cell_size,
-                             -unit_cell_size,
-                             unit_cell_size))
-    result = result.union(g_001)
-    # Octante 101
-    g_101 = g_010.translate((unit_cell_size,
-                             -unit_cell_size,
-                             unit_cell_size))
-    result = result.union(g_101)
-    # Octante 011
-    g_011 = g_100.translate((- (1 + delta) * unit_cell_size,
-                             (1 + delta) * unit_cell_size,
-                             (1 + delta) * unit_cell_size))
-    result = result.union(g_011)
-    # Octante 111
-    g_111 = g_000.translate(((1 + delta) * unit_cell_size,
-                            (1 + delta) * unit_cell_size,
-                            (1 + delta) * unit_cell_size))
-    result = result.union(g_111)
+    s_010 = s_000.mirror(mirrorPlane = "XZ",
+                            basePointVector = (0, unit_cell_size, 0))
+    result = result.union(s_010)
+    # The top side is just a mirror of the bottom one
+    s_top = result.mirror(mirrorPlane = "XY",
+                            basePointVector = (0, 0, unit_cell_size))
+    result = result.union(s_top)
     return result.val().located(location)
 cq.Workplane.unit_cell = unit_cell
 
